b7001a86934e65bb3cab414aa5033c106399f466
Murat Kardeşler <79356765+muratkardesler@users.noreply.github.com>
2025-01-10 17:43:44 +0300

    bug fix

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 const express = require("express");
 const path = require("path");
-const userRoutes = require("./routes/userRoutes.js");
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
-
-app.use(express.json());
-
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use("/api", userRoutes);
+app.use('/api', require('./routes/userRoutes'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(3000, () => {
-    console.log("Sunucu http://localhost:3000 adresinde çalışıyor");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Sunucu http://localhost:${port} adresinde çalışıyor`);
 });
