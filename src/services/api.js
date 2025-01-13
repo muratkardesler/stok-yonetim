831369833b26774a4447c42f51f298cfe82338e0
Murat Kardeşler <79356765+muratkardesler@users.noreply.github.com>
2025-01-13 16:15:04 +0300

    bug fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+// Production ve development için base URL'i belirle
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'http://flowbridge.us-e2.cloudhub.io/api'
+  : '/api';
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
@@ -12,9 +17,16 @@ const api = axios.create({
 api.interceptors.request.use(config => {
   // Client ID ve secret'ı query parametresi olarak ekle
   const params = new URLSearchParams(config.params || {});
-  params.append('client_id', process.env.VUE_APP_CLIENT_ID);
-  params.append('client_secret', process.env.VUE_APP_CLIENT_SECRET);
+  params.append('client_id', '6f0b2e5229c7455091966ef898fd6f68');
+  params.append('client_secret', '8041a365CDfb448c88a7780b7699A6aC');
   config.params = params;
+  
+  // Production ortamında CORS header'larını ekle
+  if (process.env.NODE_ENV === 'production') {
+    config.headers['Access-Control-Allow-Origin'] = '*';
+    config.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,PATCH,OPTIONS';
+    config.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+  }
   
   const token = localStorage.getItem('token');
   if (token) {
