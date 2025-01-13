7be81ee810a0a622761a599d1103b15454a6c58b
Murat Kardeşler <79356765+muratkardesler@users.noreply.github.com>
2025-01-13 16:39:00 +0300

    bug fix v1

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
+        port: 8080,
         proxy: {
             '/api': {
                 target: 'http://flowbridge.us-e2.cloudhub.io',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': '/api'
-                },
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-                    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
                 }
             }
         }
